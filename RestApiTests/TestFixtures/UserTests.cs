c6eb9aa08f262579d582b381ff81763bfd31ff89
John Doe <johndoe@example.com>
2025-01-07 14:22:40 -0700

    took out unnessary using namespace file#

---
+++ RestApiTests/TestFixtures/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using RestApiTests.Models.Users;
+using FluentAssertions;
+
+namespace RestApiTests.TestFixtures
+{
+   
+    internal class UserTests
+    {
+        [Test]
+        public void GetUsers()
+        {
+
+            var endPoint = "https://reqres.in/api/users";
+
+            var client = new RestClient(endPoint);
+            var  request = new RestRequest();
+            request.Method = Method.Get;
+
+            var response = client.Execute(request);
+
+            var responseBody = 
+                JsonConvert.DeserializeObject<UserResponse>(response.Content);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+            responseBody.page.Should().Be(1);
+            
+        }
+    }
+
+}
